5343cff51e294543e9203aceb087e19fbc768454
Bálint Aradi <aradi@uni-bremen.de>
2024-05-19 18:57:08 +0200

    Fix static compilation issues with CMake (#4)

--- cmake/FortunoCoarrayHelpers.cmake
+++ cmake/FortunoCoarrayHelpers.cmake
@@ -24,27 +24,43 @@ function (fortuno_coa_setup_build_type default_build_type)
 endfunction ()
 
 
-# Defines the coarray target for coarray dependent parts
-function (fortuno_coa_def_coarray_build_target)
+# Creates the target CoarrayBuildInterface with coarray build options
+function (fortuno_coa_create_coarray_build_target)
 
-  if (FORTUNO_COA_COARRAY_COMPILE_FLAGS)
-    set(_compiler_flags "${FORTUNO_COA_COARRAY_COMPILE_FLAGS}")
-  elseif (CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM")
-    set(_compiler_flags "-coarray")
-  elseif (CMAKE_Fortran_COMPILER_ID STREQUAL "NAG")
-    set(_compiler_flags "-coarray")
+  if (NOT TARGET CoarrayBuildInterface)
+    add_library(CoarrayBuildInterface INTERFACE)
+    target_compile_options(
+      CoarrayBuildInterface INTERFACE
+      ${FORTUNO_COA_COARRAY_COMPILE_FLAGS}
+    )
+    target_link_options(
+      CoarrayBuildInterface INTERFACE
+      ${FORTUNO_COA_COARRAY_LINK_FLAGS}
+    )
   endif ()
 
-  if (FORTUNO_COA_COARRAY_LINK_FLAGS)
-    set(_linker_flags "${FORTUNO_COA_COARRAY_LINK_FLAGS}")
-  elseif (CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM")
-    set(_linker_flags "-coarray")
-  elseif (CMAKE_Fortran_COMPILER_ID STREQUAL "NAG")
-    set(_linker_flags "-coarray")
-  endif ()
+endfunction ()
 
-  add_library(CoarrayBuild INTERFACE)
-  target_compile_options(CoarrayBuild INTERFACE ${_compiler_flags})
-  target_link_options(CoarrayBuild INTERFACE ${_linker_flags})
+
+# Applies coarray build flags to a target
+function (fortuno_coa_add_coarray_build_flags target)
+
+  fortuno_coa_create_coarray_build_target()
+
+  # TODO: Delete first branch once cmake minimum version is 3.26 or above
+  # Older CMake versions have problems during installation if the CoarrayBuildInterface target is
+  # linked directly with any target, therefore applying a workaround.
+  if (CMAKE_VERSION VERSION_LESS 3.26)
+    get_target_property(_compile_flags CoarrayBuildInterface INTERFACE_COMPILE_OPTIONS)
+    if (_compile_flags)
+      target_compile_options(${target} PRIVATE ${_compile_flags})
+    endif ()
+    get_target_property(_link_flags CoarrayBuildInterface INTERFACE_LINK_OPTIONS)
+    if (_link_flags)
+      target_link_options(${target} PRIVATE ${_link_flags})
+    endif ()
+  else ()
+    target_link_libraries(${target} PRIVATE $<BUILD_LOCAL_INTERFACE:CoarrayBuildInterface>)
+  endif ()
 
 endfunction ()
